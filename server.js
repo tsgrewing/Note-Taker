58802ed0ecec01a93004a80e7e7aa7bc9db3e5c2
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-07-21 22:50:51 -0500

    adding server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+let noteArr = [];
+
