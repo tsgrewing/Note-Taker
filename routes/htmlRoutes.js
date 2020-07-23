ea999ff311ab3d738e153023f3aa451b135d02e4
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-07-23 16:27:10 -0500

    adding index.html at "/" route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,4 +9,8 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
 };
